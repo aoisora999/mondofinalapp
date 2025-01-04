c5538a12510c73a5a71771fef1cdbf19db26b29e
yamicode999 <141847088+yamicode999@users.noreply.github.com>
2025-01-05 04:43:16 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
+      input: path.resolve(__dirname, 'index.html')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   },
   server: {
