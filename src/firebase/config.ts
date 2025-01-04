8f8df253e693f99802ce530e917565eb8a1d8b1c
yamicode999 <141847088+yamicode999@users.noreply.github.com>
2025-01-04 12:24:06 -0800

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCdOjQIKzOmfhbQSzekuRoywl05l5CVIZQ",
-    authDomain: "mondo-82764.firebaseapp.com",
-    projectId: "mondo-82764",
-    storageBucket: "mondo-82764.firebasestorage.app",
-    messagingSenderId: "1090721275889",
-    appId: "1:1090721275889:web:62384940070384accc2e50"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
